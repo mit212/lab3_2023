4fc3dc47d634ee650a41d5627cc2a8737713923e
Ravi <tejwanir@mit.edu>
2023-02-21 22:44:44 -0500

    updated motor control

--- motor_control.ino
+++ motor_control.ino
@@ -1,10 +1,11 @@
 /**********************************************************************************
 
   Arduino code to control a DC motor
-  MIT Department of Mechanical Engineering, 2.004
+  MIT Department of Mechanical Engineering, 2.12/2.120
   Rev. 1.0 -- 08/03/2020 H.C.
   Rev. 2.0 -- 08/25/2020 H.C.
   Rev. 3.0 -- 02/27/2022 H.C.
+  Rev. 4.0 -- 02/20/2023 H.C.
 
 ***********************************************************************************/
 
@@ -12,45 +13,42 @@
 #define VELOCITY_CONTROL
 //#define POSITION_CONTROL
 
+#define period_us 10000  // define sample period in microseconds (1 sec = 1000000 us)
+
 // ================================================================
 // ===               PID FEEDBACK CONTROLLER                    ===
 // ================================================================
-
 // Provide PID controller gains here
 float Kp = 0.0;
 float Ki = 0.0;
 float Kd = 0.0;
 
 // ================================================================
+// ===               SERIAL OUTPUT CONTROL                      ===
+// ================================================================
+// Switch between built-in Serial Plotter and Matlab streaming
+// comment out both to disable serial output
+
+#define arduinoSerialPrint
+//#define MATLABSerialPrint
+
+// ================================================================
 // ===               SETPOINT TYPE                              ===
 // ================================================================
+// Define setpoint (enable one of the followings)
+// disable all to use a constant setpoint
 
-// Define setpoint
+#define SQUARE_WAVE
+//#define SINE_WAVE
 
 #define desired_vel 2.5         // desired velocity in rad/s
-#define desired_pos 2*PI/3           // desired position in rad
+#define desired_pos 2*PI/3      // desired position in rad
 
 #define square_period 3000      // square wave period in milli-seconds
 #define square_on 1500          // square wave on time in milli-seconds
 
-#define sine_amp PI/2         // sine wave amplitude
-#define sine_freq_hz 0.5        // sine wave frequency in Hz
-
-#define SQUARE_WAVE
-//#define SINE_WAVE
-//#define TIC
-
-#define period_us 10000  // microseconds (1 sec = 1000000 us)
-
-// ================================================================
-// ===               SERIAL OUTPUT CONTROL                      ===
-// ================================================================
-
-// Switch between built-in Serial Plotter and Matlab streaming
-// comment out both to disable serial output
-
-#define PRINT_DATA
-//#define MATLAB_SERIAL_READ
+#define sine_amp PI/2           // sine wave amplitude
+#define sine_freq_hz 1.0        // sine wave frequency in Hz
 
 // ================================================================
 // ===               DFROBOT MOTOR SHIELD DEFINITION            ===
@@ -81,23 +79,21 @@ volatile long counter = 0;    // Inrerrupt counter (position)
 // ================================================================
 // ===               VARIABLE DEFINITION                        ===
 // ================================================================
-
 #define vc2pwm 51.0    // pwm = 255 corresponds to 5v from pwm pin (255/5)
 
 float wheel_pos = 0.0, wheel_vel = 0.0, pre_wheel_pos = 0.0, filt_vel = 0.0;
-float error, sum_error = 0.0, d_error = 0.0, filt_d_error = 0.0, error_pre;
+float error, i_error = 0.0, d_error = 0.0, filt_d_error = 0.0, error_pre;
 float alpha = 0.5;    // low pass filter constant
 unsigned long timer;
-double loop_time = 0.01;
-float Pcontrol, Icontrol, Dcontrol;
-float pwm;
-float vc;
-float set_point = desired_vel, sensor;
+double loop_time = 0.01;  // sample time in seconds
+float Pcontrol, Icontrol, Dcontrol; // control actions
+float pwm = 0;  // PWM value (integer between -255 and 255)
+float vc; // PWM voltage in volts
+float set_point = desired_vel, sensor;  // initial set point
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
-
 void setup() {
 
   Serial.begin(115200);
@@ -122,15 +118,13 @@ void setup() {
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
-
 void loop() {
-  if (micros() - timer >= period_us) {
+if (micros() - timer >= period_us) {
   timer = micros();
-  count = counter;
+  count = counter;  // get encoder counts from interrupt service routines
 
   // wheel position and velocity from the encoder counts
-  wheel_pos = count;
-  wheel_pos = wheel_pos / C2Rad;    // convert to radians
+  wheel_pos = count / C2Rad;    // convert to radians
   wheel_vel = (wheel_pos - pre_wheel_pos) / loop_time;
   filt_vel = alpha * wheel_vel + (1 - alpha) * filt_vel;
   pre_wheel_pos = wheel_pos;
@@ -138,34 +132,27 @@ void loop() {
   // ================================================================
   // ===                    GET SETPOINT                          ===
   // ================================================================
-
   // define set point as a square wave input
-#ifdef SQUARE_WAVE
-  if (millis() % square_period < square_on){
-   #ifdef VELOCITY_CONTROL
-    set_point = desired_vel;
-  #endif
-  #ifdef POSITION_CONTROL
-    set_point = desired_pos;
-  #endif
-  }
-  else
+  #ifdef SQUARE_WAVE
+  if (millis() % square_period < square_on) {
+    #ifdef VELOCITY_CONTROL
+      set_point = desired_vel;
+    #endif
+    #ifdef POSITION_CONTROL
+      set_point = desired_pos;
+    #endif
+  } else
     set_point = 0.0;
-#endif
+  #endif
 
   // define set point as a sine wave with an amplitude and frequency
-#ifdef SINE_WAVE
-  set_point = sine_amp * sin(sine_freq_hz * (2 * M_PI) * millis() / 1000.0);
-#endif
-
-#ifdef TIC
-  set_point = millis() / 1000 * PI / 6;
-#endif
+  #ifdef SINE_WAVE
+    set_point = sine_amp * sin(sine_freq_hz * (2 * M_PI) * millis() / 1000.0);
+  #endif
 
   // ================================================================
   // ===                    CONTROLLER CODE                       ===
   // ================================================================
-
   // PID controller code
   #ifdef VELOCITY_CONTROL
     sensor = filt_vel;
@@ -178,10 +165,10 @@ void loop() {
   d_error = (error - error_pre) / loop_time;
   filt_d_error = alpha * d_error + (1 - alpha) * filt_d_error;
   error_pre = error;
-  sum_error += error * loop_time;
+  i_error += error * loop_time;
 
   Pcontrol = error * Kp;
-  Icontrol = sum_error * Ki;
+  Icontrol = i_error * Ki;
   Dcontrol = d_error * Kd;    // use d_error to avoid delay
 
   Icontrol = constrain(Icontrol, -255, 255);    // anti-windup for integrator
@@ -194,10 +181,9 @@ void loop() {
   // ================================================================
   // ===                    MOTOR PWM CODE                        ===
   // ================================================================
+  vc = constrain(pwm / vc2pwm, -5, 5);  // pwm pin voltage
 
-  vc = constrain(pwm / vc2pwm, -5, 5);
-
-  // set the correct direction
+  // set the direction of rotation: LOW - forward; HIGH - backward
   if (pwm > 0) {
     digitalWrite(DIR_A, HIGH);
   }
@@ -205,12 +191,10 @@ void loop() {
     digitalWrite(DIR_A, LOW);
   }
 
-  // constrain pwm to +/- 230
-  pwm = constrain(pwm, -255, 255);
+  pwm = constrain(pwm, -255, 255);  // constrain pwm to +/- 255
+  analogWrite(PWM_A, (int) abs(pwm));     // output the pwm
 
-  analogWrite(PWM_A, (int) abs(pwm));     // Set speed
-
-#ifdef PRINT_DATA
+#ifdef arduinoSerialPrint
   Serial.print(set_point);  Serial.print("\t");
   Serial.print(sensor); Serial.print("\t");
   //    Serial.print(filt_vel); Serial.print("\t");
@@ -219,18 +203,16 @@ void loop() {
   Serial.print(vc);  Serial.print("\t");
   //    Serial.print(pwm);  Serial.print("\t");
   //    Serial.print(loop_time,6); Serial.print("\t");
-  Serial.print("\n");
+  Serial.println();
 #endif
 
-#ifdef MATLAB_SERIAL_READ
+#ifdef MATLABSerialPrint
   Serial.print(timer / 1000000.0);   Serial.print("\t");
   Serial.print(set_point);    Serial.print("\t");
   Serial.print(sensor);    Serial.print("\t");
   Serial.print(vc);
-  Serial.print("\n");
+  Serial.println();
 #endif
-
-  //  delay(8);
   }
   loop_time = (micros() - timer) / 1000000.0;  //compute actual sample time
 }
